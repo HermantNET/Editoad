04b11bbe825d2b81dd0b61ecdb56a66a083f97a9
HermantNET <hermant.edu@gmail.com>
2017-11-08 17:15:03 +0800

    added dist build webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack")
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "./dist/editoad.js",
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["env", "react", "flow", "stage-0"],
+        },
+      },
+      {
+        test: /\.s?css$/,
+        loader: "style-loader!css-loader",
+      },
+    ],
+  },
+  externals: {
+    // Use external version of React
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+}
