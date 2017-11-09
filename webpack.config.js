a10f53b8a70aed63f183d73ca97335bc140a6bcb
HermantNET <hermant.edu@gmail.com>
2017-11-09 21:45:03 +0800

    Modified main, output compiled dist

--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,16 @@ module.exports = {
       },
     ],
   },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom",
+    "react-dnd": "react-dnd",
+    jest: "jest",
+    "material-ui": "material-ui",
+    "react-redux": "react-redux",
+    redux: "redux",
+    "redux-thunk": "redux-thunk",
+    "react-tap-event-plugin": "react-tap-event-plugin",
+    "react-dnd-html5-backend": "react-dnd-html5-backend",
+  }
 }
